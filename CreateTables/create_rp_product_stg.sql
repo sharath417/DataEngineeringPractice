279a38d497e1f9b4690697a46b9394f13a154763
sharath417 <sharathchand417@gmail.com>
2025-12-27 18:42:57 +0530

    Add files via upload

---
+++ CreateTables/create_rp_product_stg.sql
@@ -0,0 +1,15 @@
+create table rp_product_stg_ds.rp_product_stg_table(
+	cid STRING,
+	upc STRING,
+	oid STRING,
+	dt STRING,
+	r_dt STRING,
+	prc STRING,
+	qty STRING,
+	amt STRING,
+	r_qty STRING,
+	r_amt STRING,
+	web_prod_id STRING,
+	GMM_DESC STRING,
+	PARENT_MDSE_DIVN_DESC STRING
+);
